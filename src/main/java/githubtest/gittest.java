625c406f38dec3326274bb6bf76b682fcdfeefe0
liuchao1026 <734362851@qq.com>
2016-09-29 19:46:41 +0800

    gegeggeg

---
+++ src/main/java/githubtest/gittest.java
@@ -0,0 +1,11 @@
+package githubtest;
+
+/**
+ * Created by Administrator on 2016/9/29.
+ */
+public class gittest {
+    public static void main(String[] args) {
+        //这是我第一次提交项目github项目啊
+    }
+
+}
